a28374b39a2252c837298226d51007e6411ee453
Karacky <mrnk3078@gmail.com>
2021-03-09 01:07:50 +0900

    feat: 送られてきたmessageをstreamで受信・レスポンスする

--- server/main.go
+++ server/main.go
@@ -1,28 +1,70 @@
 package main
 
 import (
+	"fmt"
+	"io"
 	"log"
-	"net/http"
+	"net"
 	"os"
 
-	"github.com/labstack/echo"
-	"github.com/labstack/echo/middleware"
+	pb "github.com/karamaru-alpha/grpc-sample/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
+type server struct {
+	pb.ChatServer
+}
+
+func (s *server) BiliChat(stream pb.Chat_BiliChatServer) error {
+	log.Println("start new server in BiliChat!")
+	ctx := stream.Context()
+
+	for {
+		// チャネルが終了した場合return。それ以外(値が入っていない場合も)処理を続行する。
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		default:
+		}
+
+		// Streamから値を受け取る
+		req, err := stream.Recv()
+		// タイムアウト時などはエラーがEOFで来るらしい
+		if err == io.EOF {
+			log.Println("exit")
+			return nil
+		}
+		if err != nil {
+			log.Printf("receive error %v", err)
+			continue
+		}
+
+		resp := pb.MessageResponse{Message: req.Message}
+		if err := stream.Send(&resp); err != nil {
+			log.Printf("send error %v", err)
+		}
+		log.Printf("send message=%s", req.Message)
+	}
+}
+
 func main() {
-	e := echo.New()
-	e.Use(
-		middleware.Logger(),
-		middleware.Recover(),
-	)
+	fmt.Print()
 
-	// TODO: routeは別ディレクトリに切り出す
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello World")
-	})
+	// リスナー登録
+	lis, err := net.Listen("tcp", ":"+os.Getenv("PORT"))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
 
-	log.Println("Server running...")
-	if err := e.Start(":" + os.Getenv("PORT")); err != nil {
-		log.Fatalf("Listen and serve failed. %+v", err)
+	// gRPC作成
+	s := grpc.NewServer()
+	pb.RegisterChatServer(s, &server{})
+
+	// サーバ起動
+	log.Printf("Server running in port: %s", os.Getenv("PORT"))
+	reflection.Register(s)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
 	}
 }
